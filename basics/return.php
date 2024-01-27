7349ed815228f1cd794ca5f4b70c9f598c4031e0
Vedant Vankar <vedantvanpro@gmail.com>
2024-01-27 18:59:42 +0530

    return statements

---
+++ basics/return.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>return</title>
+</head>
+
+<body>
+    <?php
+    function cube($num)
+    {
+        return $num * $num * $num;
+    }
+
+    $cubeResult = cube(4);
+    echo $cubeResult;
+    ?>
+</body>
+
+</html>
