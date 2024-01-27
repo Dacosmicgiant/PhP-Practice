a2d576eb108f02d1563950f99a4bdc6d05c54566
Vedant Vankar <vedantvanpro@gmail.com>
2024-01-27 16:55:51 +0530

    arrays

---
+++ arrays.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+
+<body>
+
+    <?php
+    $friends = array("Yash", "Dipesh", "Geet", "Sanch", "Bibhu");
+    $friends[3] = "Sancho";
+    echo "$friends[3]";
+    echo count($friends);
+    ?>
+
+</body>
+
+</html>
