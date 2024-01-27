7a7d987c35cb312bd957ee2d35a380bd81ca8cea
Vedant Vankar <vedantvanpro@gmail.com>
2024-01-28 00:36:58 +0530

    classes and objects

---
+++ basics/includeFunction/site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>including php</title>
+</head>
+
+<body>
+    <?php
+    $title = "my first post";
+    $author = "Vedant";
+    $wordCount = 400;
+    include("article-header.php");
+    ?>
+</body>
+
+</html>
