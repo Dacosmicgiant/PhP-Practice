1f13ef16a3b466393adbfffd8b18da565c0c2e16
Vedant Vankar <vedantvanpro@gmail.com>
2024-01-28 12:18:25 +0530

    inheritance

---
+++ basics/inheritance.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inheritance</title>
+</head>
+
+<body>
+    <?php
+    class Chef
+    {
+        function makeChicken()
+        {
+            echo "the chef makes chicken <br>";
+        }
+        function makeSalad()
+        {
+            echo "The chef makes salad <br>";
+        }
+        function makeSpecialDish()
+        {
+            echo "The chef makes bbq ribs";
+        }
+
+    }
+
+    class ItalianChef extends Chef
+    {
+        function makePasta()
+        {
+            echo "The chef makes pasta<br>";
+        }
+    }
+
+    $chef = new Chef();
+    $chef->makeChicken();
+
+    $italianChef = new ItalianChef();
+    $italianChef->makePasta();
+    $italianChef->makeChicken();
+
+    ?>
+</body>
+
+</html>
